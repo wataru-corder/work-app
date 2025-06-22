8cb49aaab6faeddf8839067ff8789a7b8081a176
wataru <wataru.corder@gmail.com>
2025-06-22 22:21:37 +0900

    不要なAPI関数を削除し、コードをクリーンアップ

--- lib/api.ts
+++ lib/api.ts
@@ -9,28 +9,3 @@ export async function fetchWorkouts(): Promise<Workout[]> {
   }
   return res.json()
 }
-
-export async function crateWorkouts(date: Workout): Promise<Workout[]> {
-  const res = await fetch(`${API_URL}/workouts`, {
-    method: 'POST',
-    headers: {
-      'Content-Type': 'application/json',
-    },
-    body: JSON.stringify(date),
-  })
-  if (!res.ok) {
-    throw new Error('作成に失敗')
-  }
-  return res.json()
-}
-
-export async function updateWorkouts(
-  id: string,
-  date: Workout
-): Promise<Workout[]> {
-  const res = await fetch(`${API_URL}/workouts`)
-  if (!res.ok) {
-    throw new Error('取得失敗')
-  }
-  return res.json()
-}
