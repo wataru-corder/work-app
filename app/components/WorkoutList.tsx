20c2ef3a5b7361ccbd481dc23a2e8a5ee6639b68
wataru <wataru.corder@gmail.com>
2025-06-28 13:52:37 +0900

    WorkoutListコンポーネントに日付表示を追加し、編集・削除ボタンを実装。

--- app/components/WorkoutList.tsx
+++ app/components/WorkoutList.tsx
@@ -1,4 +1,5 @@
 import { Workout } from '@/types'
+import { Pencil, Trash2 } from 'lucide-react'
 
 type workoutListProps = {
   workout: Workout[]
@@ -8,26 +9,40 @@ const WorkoutList = ({ workout }: workoutListProps) => {
   if (workout.length === 0) {
     return <p className="text-gray-700">今日の記録はまだありません</p>
   }
+
   return (
     <div>
-      <div className="text-xl font-bold mb-4">今日の記録 </div>
+      <div className="text-xl font-bold mb-4">
+        {new Date(workout[0].date).toLocaleDateString('ja-JP', {
+          month: 'long',
+          day: 'numeric',
+        })}
+        の記録
+      </div>
       <div>
-        {workout.map((workout) => (
-          <div
-            key={workout.id}
-            className="bg-white rounded-2xl shadow-lg p-6 border border-gray-100"
-          >
-            <p className="font-semibold">{workout.date}</p>
-            <ul className="mt-2 space-y-1 text-sm">
+        <div className="bg-white rounded-2xl shadow-lg p-6 border border-gray-100">
+          {workout.map((workout) => (
+            <ul className="mt-2 space-y-1 font-semibold" key={workout.id}>
               {workout.records.map((r) => (
-                <li key={r.id}>
+                <li
+                  key={r.id}
+                  className="pb-2 flex justify-between items-center"
+                >
                   {r.exercise}：{r.weight}kg × {r.reps}回 × {r.sets}セット
                   {r.memo}
+                  <span className="flex gap-4 text-gray-500">
+                    <button className=" hover:text-gray-800 cursor-pointer">
+                      <Pencil size={16} />
+                    </button>
+                    <button className="hover:text-gray-800 cursor-pointer">
+                      <Trash2 size={16} />
+                    </button>
+                  </span>
                 </li>
               ))}
             </ul>
-          </div>
-        ))}
+          ))}
+        </div>
       </div>
     </div>
   )
