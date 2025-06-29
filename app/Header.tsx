5382074b00b94bed93add8479d1097ea5a4dd250
wataru <wataru.corder@gmail.com>
2025-06-30 00:34:55 +0900

    Headerコンポーネントのタイトルを「Logo」から「Fitness Journey」に変更し、幅のスタイルを調整

--- app/Header.tsx
+++ app/Header.tsx
@@ -5,8 +5,8 @@ const Header = () => {
   return (
     <div>
       <header className="border-b border-gray-400">
-        <div className="w-[75%] mx-auto flex justify-between items-center  px-10 h-20">
-          <h1 className="font-bold text-xl">Logo</h1>
+        <div className="w-[95%] w-max[1400px]mx-auto flex justify-between items-center  px-10 h-20">
+          <h1 className="font-bold text-xl">Fitness Journey</h1>
           <div>
             <Button>ログアウト</Button>
           </div>
